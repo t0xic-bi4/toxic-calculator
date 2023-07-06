018605b445677d8991464de90bf1d91bcff38a8c
t0xic-bi4 <138766923+t0xic-bi4@users.noreply.github.com>
2023-07-06 19:08:25 +1000

    f[gf[g[fg[

---
+++ toxic/calculator/Form1.Designer.cs
@@ -0,0 +1,416 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button22 = new Button();
+            button21 = new Button();
+            button23 = new Button();
+            button24 = new Button();
+            button20 = new Button();
+            button19 = new Button();
+            button18 = new Button();
+            button17 = new Button();
+            button16 = new Button();
+            button15 = new Button();
+            button14 = new Button();
+            button13 = new Button();
+            button12 = new Button();
+            button11 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button22);
+            panel1.Controls.Add(button21);
+            panel1.Controls.Add(button23);
+            panel1.Controls.Add(button24);
+            panel1.Controls.Add(button20);
+            panel1.Controls.Add(button19);
+            panel1.Controls.Add(button18);
+            panel1.Controls.Add(button17);
+            panel1.Controls.Add(button16);
+            panel1.Controls.Add(button15);
+            panel1.Controls.Add(button14);
+            panel1.Controls.Add(button13);
+            panel1.Controls.Add(button12);
+            panel1.Controls.Add(button11);
+            panel1.Controls.Add(button10);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(413, 653);
+            panel1.TabIndex = 0;
+            panel1.Click += panel1_Click_1;
+            panel1.Paint += panel1_Paint;
+            // 
+            // button22
+            // 
+            button22.Location = new Point(23, 569);
+            button22.Margin = new Padding(2);
+            button22.Name = "button22";
+            button22.Size = new Size(73, 71);
+            button22.TabIndex = 25;
+            button22.Text = "+/-";
+            button22.UseVisualStyleBackColor = true;
+            button22.Click += button22_Click;
+            // 
+            // button21
+            // 
+            button21.Location = new Point(23, 194);
+            button21.Margin = new Padding(2);
+            button21.Name = "button21";
+            button21.Size = new Size(73, 77);
+            button21.TabIndex = 24;
+            button21.Text = "1/x";
+            button21.UseVisualStyleBackColor = true;
+            button21.Click += button21_Click;
+            // 
+            // button23
+            // 
+            button23.Location = new Point(220, 194);
+            button23.Margin = new Padding(2);
+            button23.Name = "button23";
+            button23.Size = new Size(73, 77);
+            button23.TabIndex = 22;
+            button23.Text = "sqrt";
+            button23.UseVisualStyleBackColor = true;
+            button23.Click += button23_Click;
+            // 
+            // button24
+            // 
+            button24.Location = new Point(119, 194);
+            button24.Margin = new Padding(2);
+            button24.Name = "button24";
+            button24.Size = new Size(75, 77);
+            button24.TabIndex = 21;
+            button24.Text = "x^2";
+            button24.UseVisualStyleBackColor = true;
+            button24.Click += button24_Click;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(23, 101);
+            button20.Margin = new Padding(2);
+            button20.Name = "button20";
+            button20.Size = new Size(73, 77);
+            button20.TabIndex = 20;
+            button20.Text = "%";
+            button20.UseVisualStyleBackColor = true;
+            button20.Click += textBox1_Click;
+            // 
+            // button19
+            // 
+            button19.Location = new Point(319, 101);
+            button19.Margin = new Padding(2);
+            button19.Name = "button19";
+            button19.Size = new Size(73, 77);
+            button19.TabIndex = 19;
+            button19.Text = "del";
+            button19.UseVisualStyleBackColor = true;
+            button19.Click += button19_Click;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(121, 288);
+            button18.Margin = new Padding(2);
+            button18.Name = "button18";
+            button18.Size = new Size(73, 76);
+            button18.TabIndex = 18;
+            button18.Text = "2";
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += panel1_Click;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(220, 288);
+            button17.Margin = new Padding(2);
+            button17.Name = "button17";
+            button17.Size = new Size(73, 76);
+            button17.TabIndex = 17;
+            button17.Text = "3";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += panel1_Click;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(319, 199);
+            button16.Margin = new Padding(2);
+            button16.Name = "button16";
+            button16.Size = new Size(73, 76);
+            button16.TabIndex = 16;
+            button16.Text = "/";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += textBox1_Click;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(21, 382);
+            button15.Margin = new Padding(2);
+            button15.Name = "button15";
+            button15.Size = new Size(75, 71);
+            button15.TabIndex = 15;
+            button15.Text = "4";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += panel1_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(121, 382);
+            button14.Margin = new Padding(2);
+            button14.Name = "button14";
+            button14.Size = new Size(73, 71);
+            button14.TabIndex = 14;
+            button14.Text = "5";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += panel1_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(21, 475);
+            button13.Margin = new Padding(2);
+            button13.Name = "button13";
+            button13.Size = new Size(75, 73);
+            button13.TabIndex = 13;
+            button13.Text = "7";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += panel1_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(121, 475);
+            button12.Margin = new Padding(2);
+            button12.Name = "button12";
+            button12.Size = new Size(73, 73);
+            button12.TabIndex = 12;
+            button12.Text = "8";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += panel1_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(220, 569);
+            button11.Margin = new Padding(2);
+            button11.Name = "button11";
+            button11.Size = new Size(75, 71);
+            button11.TabIndex = 11;
+            button11.Text = ",";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(121, 569);
+            button10.Margin = new Padding(2);
+            button10.Name = "button10";
+            button10.Size = new Size(73, 71);
+            button10.TabIndex = 10;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += panel1_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(319, 569);
+            button9.Margin = new Padding(2);
+            button9.Name = "button9";
+            button9.Size = new Size(73, 71);
+            button9.TabIndex = 9;
+            button9.Text = "=";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(319, 293);
+            button8.Margin = new Padding(2);
+            button8.Name = "button8";
+            button8.Size = new Size(73, 71);
+            button8.TabIndex = 8;
+            button8.Text = "X";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += textBox1_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(220, 475);
+            button7.Margin = new Padding(2);
+            button7.Name = "button7";
+            button7.Size = new Size(73, 73);
+            button7.TabIndex = 7;
+            button7.Text = "9";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += panel1_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(319, 475);
+            button6.Margin = new Padding(2);
+            button6.Name = "button6";
+            button6.Size = new Size(73, 73);
+            button6.TabIndex = 6;
+            button6.Text = "+";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += textBox1_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(319, 382);
+            button5.Margin = new Padding(2);
+            button5.Name = "button5";
+            button5.Size = new Size(73, 71);
+            button5.TabIndex = 5;
+            button5.Text = "-";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += textBox1_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(220, 382);
+            button4.Margin = new Padding(2);
+            button4.Name = "button4";
+            button4.Size = new Size(73, 71);
+            button4.TabIndex = 4;
+            button4.Text = "6";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += panel1_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(21, 288);
+            button3.Margin = new Padding(2);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 76);
+            button3.TabIndex = 3;
+            button3.Text = "1";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += panel1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(220, 101);
+            button2.Margin = new Padding(2);
+            button2.Name = "button2";
+            button2.Size = new Size(73, 77);
+            button2.TabIndex = 2;
+            button2.Text = "C";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += panel1_Click_1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(119, 101);
+            button1.Margin = new Padding(2);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 77);
+            button1.TabIndex = 1;
+            button1.Text = "CE";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += panel1_Click_1;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Info;
+            textBox1.Location = new Point(21, 41);
+            textBox1.Margin = new Padding(2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(373, 27);
+            textBox1.TabIndex = 0;
+            textBox1.Click += textBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(413, 653);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(2);
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "t0x1c-calculator";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private TextBox textBox1;
+        private Button button20;
+        private Button button19;
+        private Button button18;
+        private Button button17;
+        private Button button16;
+        private Button button15;
+        private Button button14;
+        private Button button13;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button21;
+        private Button button23;
+        private Button button24;
+        private Button button22;
+    }
+}
---
+++ toxic/calculator/Form1.cs
@@ -0,0 +1,132 @@
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public string D;
+        public string N1;
+        public bool N2;
+        public Form1()
+        {
+            N2 = false;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            if (N2)
+            {
+                N2 = false;
+                textBox1.Text = "0";
+            }
+            Button B = (Button)sender;
+            if (textBox1.Text == "0")
+                textBox1.Text = B.Text;
+            else
+                textBox1.Text = textBox1.Text + B.Text;
+        }
+
+        private void panel1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            D = B.Text;
+            N1 = textBox1.Text;
+            N2 = true;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            double dn1, dn2, res;
+            res = 0;
+            dn1 = Convert.ToDouble(N1);
+            dn2 = Convert.ToDouble(textBox1.Text);
+            if (D == "+")
+            {
+                res = dn1 + dn2;
+            }
+            if (D == "-")
+            {
+                res = dn1 - dn2;
+            }
+            if (D == "X")
+            {
+                res = dn1 * dn2;
+            }
+            if (D == "/")
+            {
+                res = dn1 / dn2;
+            }
+            if (D == "%")
+            {
+                res = (dn1 / dn2) * 100;
+            }
+            D = "=";
+            N2 = true;
+            textBox1.Text = res.ToString();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Contains(","))
+                textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+            if (textBox1.Text == "")
+                textBox1.Text = "0";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            double dn, res;
+            dn = Convert.ToDouble(textBox1.Text);
+            res = Math.Sqrt(dn);
+            textBox1.Text = res.ToString();
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            double dn, res;
+            dn = Convert.ToDouble(textBox1.Text);
+            res = Math.Pow(dn, 2);
+            textBox1.Text = res.ToString();
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            double dn, res;
+            dn = Convert.ToDouble(textBox1.Text);
+            res = 1 / dn;
+            textBox1.Text = res.ToString();
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            double dn, res;
+            dn = Convert.ToDouble(textBox1.Text);
+            res = -dn;
+            textBox1.Text = res.ToString();
+        }
+    }
+}
